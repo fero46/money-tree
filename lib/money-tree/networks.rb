a15faf624165976b87bbfe0218b5f2796c7cbf12
Tim Kretschmer <krtschmr@users.noreply.github.com>
2019-05-20 07:51:03 +0700

    added litecoin support

--- lib/money-tree/networks.rb
+++ lib/money-tree/networks.rb
@@ -27,7 +27,32 @@ module MoneyTree
           compressed_wif_chars: %w(c),
           uncompressed_wif_chars: %w(9),
           protocol_version: 70001
+        }, 
+        litecoin: {
+          address_version: '30',
+          p2sh_version: '32',
+          p2sh_char: '3',##TODO
+          privkey_version: 'b0',
+          privkey_compression_flag: '01',##TODO
+          extended_privkey_version: "019d9cfe",
+          extended_pubkey_version: "019da462",
+          compressed_wif_chars: %w(K L),##TODO
+          uncompressed_wif_chars: %w(5),##TODO
+          protocol_version: 70002
+        },
+        litecoin_testnet: {
+          address_version: '6f',
+          p2sh_version: '3a',
+          p2sh_char: '2',##TODO
+          privkey_version: 'ef',
+          privkey_compression_flag: '01',##TODO
+          extended_privkey_version: "0436ef7d",
+          extended_pubkey_version: "0436f6e1",
+          compressed_wif_chars: %w(c),##TODO
+          uncompressed_wif_chars: %w(9),##TODO
+          protocol_version: 70002
         }
+        
       )
       hsh[:testnet3] = hsh[:bitcoin_testnet]
       hsh
